5fae07ce9d3bf1a9131d9ea15dac191c84919f3a
willianson5522 <williansoncesar@gmail.com>
2022-06-01 21:26:31 -0300

    implementado colisao com pipe

--- mario.js
+++ mario.js
@@ -10,14 +10,22 @@ const jump = () => {
 }
 
 const loop = setInterval(() => {
+
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    console.log(marioPosition)
-
-    if (pipePosition <= 57) {
+    if (pipePosition <= 57 && pipePosition > 0 && marioPosition < 41) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = 'img/game-over.png'
+        mario.style.width = '50px'
+        mario.style.marginLeft = '10px'
+
+        clearInterval(loop);
     }
 }, 10)
 
